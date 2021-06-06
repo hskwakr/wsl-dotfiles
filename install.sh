237e189d1583ce55c905a66f9fa228698f4ea9a8
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-06-06 15:28:19 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,6 +31,7 @@ if [ ! -d ${DOT_DIR} ]; then
 		[[ "$f" == ".gitignore" ]] && continue
 		[[ "$f" == ".gitattributes" ]] && continue
 		ln -snf $DOT_DIR/"$f" $HOME/"$f"
+		echo "Installed $f"
 	done
 else
 	echo "dotfiles already exists"
