fa937f4fe0aacc45422734e8b496c8d9db8d86d5
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-06-12 21:16:59 +0900

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,19 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
 # Remove dotfiles and this repository
-DOT_DIR="$HOME/wsl-dotfiles"
-if [ -d ${DOT_DIR} ]; then
-	cd ${DOT_DIR}
+DOT_DIR="${HOME}/wsl-dotfiles"
+if [ -d "${DOT_DIR}" ]; then
+	cd "${DOT_DIR}" || exit 1
 	echo "Remove linked dot files from dotfiles repository..."
 	for f in .??*
 	do
-		[[ "$f" == ".git" ]] && continue
-		[[ "$f" == ".gitignore" ]] && continue
-		[[ "$f" == ".gitattributes" ]] && continue
+		[ "$f" "=" ".git" ] && continue
+		[ "$f" "=" ".gitignore" ] && continue
+		[ "$f" "=" ".gitattributes" ] && continue
 
-		rm -rvf "$HOME/$f"
+		rm -rvf "${HOME:?}/$f"
 	done
-	rm -rf ${DOT_DIR} 
+	rm -rf "${DOT_DIR:?}"
 else
 	echo "dotfiles does not exists"
 	exit 1
