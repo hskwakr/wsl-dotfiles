fbb4a43b46878c52c0e2bc3ea298a0d522846ab5
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-06-15 23:09:09 +0900

    Create list.sh

---
+++ list.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Show list of dotfiles
+DOT_DIR="${HOME}/wsl-dotfiles"
+if [ -d "${DOT_DIR}" ]; then
+	cd "${DOT_DIR}" || exit 1
+	echo "Show list of dotfiles in dotfiles repository..." 
+	for f in .??*; do
+		[ "$f" "=" ".git" ] && continue
+		[ "$f" "=" ".github" ] && continue
+		[ "$f" "=" ".gitignore" ] && continue
+		[ "$f" "=" ".gitattributes" ] && continue
+
+		echo "${HOME}/$f"
+	done
+else
+	echo "dotfiles does not exists"
+	exit 1
+fi
