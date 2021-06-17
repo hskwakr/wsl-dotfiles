b5e233359ab3a145781ae0ba599461558303d4ef
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-06-17 09:58:48 +0900

    Create clean-vim-plug.sh

---
+++ etc/init-clean/clean-vim-plug.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# https://github.com/junegunn/vim-plug
+
+CLEAN_DIR="$HOME/.vim/autoload"
+REMOVE_ITEM="${CLEAN_DIR}/plug.vim"
+
+if [ -d "${CLEAN_DIR}" ]; then
+	echo "Remove ${REMOVE_ITEM} ..."
+	rm -rf "${REMOVE_ITEM:?}"
+	echo "done."
+else
+	echo "${CLEAN_DIR} does not exist"
+	exit 1
+fi
