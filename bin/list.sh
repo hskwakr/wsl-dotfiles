d4977f3bd327a2a685fae419f9ca724e2ed07aff
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-06-17 10:27:29 +0900

    Update list.sh

--- bin/list.sh
+++ bin/list.sh
@@ -4,12 +4,13 @@
 DOT_DIR="${HOME}/wsl-dotfiles"
 if [ -d "${DOT_DIR}" ]; then
 	cd "${DOT_DIR}" || exit 1
-	echo "Show list of dotfiles in dotfiles repository..."
+	echo "Show list of available dotfiles in dotfiles repository..."
 	for f in .??*; do
 		[ "$f" "=" ".git" ] && continue
 		[ "$f" "=" ".github" ] && continue
 		[ "$f" "=" ".gitignore" ] && continue
 		[ "$f" "=" ".gitattributes" ] && continue
+		[ "$f" "=" ".cache" ] && continue
 
 		echo "${HOME}/$f"
 	done
