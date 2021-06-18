54d08001de4bf8e469108027e55bbbffa8bd1057
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-06-18 09:20:53 +0900

    Update init-clean.sh

--- bin/init-clean.sh
+++ bin/init-clean.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-SCRIPTS_DIR="$HOME/wsl-dotfiles/etc/init-clean"
+SCRIPTS_DIR="$HOME/wsl-dotfiles/etc/clean"
 
 if [ -d "${SCRIPTS_DIR}" ]; then
-	echo "Start runing init-clean scripts..."
+	echo "Start runing clean scripts..."
 	cd "${SCRIPTS_DIR}" || exit 1
 	for f in *.sh; do
 		echo "Run ${f}..."
