e12845ba6391720b27ed8eefec314be4ad251468
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-06-19 00:55:16 +0900

    Update clean-vim-plug.sh

--- etc/clean/clean-vim-plug.sh
+++ etc/clean/clean-vim-plug.sh
@@ -7,7 +7,6 @@ REMOVE_ITEM="${CLEAN_DIR}/plug.vim"
 if [ -d "${CLEAN_DIR}" ]; then
 	echo "Remove ${REMOVE_ITEM} ..."
 	rm -rf "${REMOVE_ITEM:?}"
-	echo "done."
 else
 	echo "${CLEAN_DIR} does not exist"
 	exit 1
