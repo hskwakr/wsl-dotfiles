6800189b63df20cb0a550f29b77e2c804a1554ce
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-06-21 11:38:12 +0900

    Add vivid

---
+++ etc/clean/clean-vivid.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Remove vivid
+
+DOT_DIR="$HOME/wsl-dotfiles"
+. "${DOT_DIR}/etc/lib/sh/has.sh"
+
+if has "vivid"; then
+	echo "Remove vivid ..."
+
+	sudo dpkg -r vivid
+else
+	echo "vivid is already removed"
+	exit 1
+fi
