18f3f1a6388a544edd31c8c0af3684cdbecdc52d
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-06-22 05:55:04 +0900

    Create install-git.sh

---
+++ etc/init/install-git.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Install git with a way following below site.
+# https://git-scm.com/download/linux 
+
+DOT_DIR="$HOME/wsl-dotfiles"
+. "${DOT_DIR}/etc/lib/sh/has.sh"
+
+printf "\n\nStart installing git ..."
+if ! has "git"; then
+	sudo apt update
+	sudo apt install git
+fi
