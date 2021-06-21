af53f42c339062aaa6d17c18ddb0dc6ad0da9ca3
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-06-21 10:14:18 +0900

    Update install-fd.sh

--- etc/init/install-fd.sh
+++ etc/init/install-fd.sh
@@ -3,16 +3,21 @@
 # https://github.com/sharkdp/fd#on-ubuntu
 
 DOT_DIR="$HOME/wsl-dotfiles"
-INSTALL_ITEM="${DOT_DIR}/.cache/fd.deb"
+INSTALL_DIR="${DOT_DIR}/.cache"
+INSTALL_ITEM="${INSTALL_DIR}/fd.deb"
 . "${DOT_DIR}/etc/lib/sh/has.sh"
 
 if ! has "fd"; then
-	echo "Start installing fd ..."
+	if [ ! -d "${INSTALL_DIR}" ]; then
+		mkdir -p "${INSTALL_DIR}"
+	fi
 
 	if ! has "wget"; then
 		echo "wget required"
 		exit 1
 	fi
+
+	echo "Start installing fd ..."
 
 	wget -O "${INSTALL_ITEM}" "https://github.com/sharkdp/fd/releases/download/v8.2.1/fd_8.2.1_amd64.deb"
 	# adapt version number and architecture
