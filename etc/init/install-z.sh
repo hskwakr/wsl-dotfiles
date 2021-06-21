1da98d0a6ba1fc93d15255bfa5401db44b643c33
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-06-21 10:16:36 +0900

    Update install-z.sh

--- etc/init/install-z.sh
+++ etc/init/install-z.sh
@@ -3,16 +3,21 @@
 # https://github.com/rupa/z
 
 DOT_DIR="$HOME/wsl-dotfiles"
-INSTALL_ITEM="${DOT_DIR}/.cache/z.sh"
+INSTALL_DIR="${DOT_DIR}/.cache"
+INSTALL_ITEM="${INSTALL_DIR}/z.sh"
 . "${DOT_DIR}/etc/lib/sh/has.sh"
 
 if ! has "z"; then
-	echo "Start installing z ..."
+	if [ ! -d "${INSTALL_DIR}" ]; then
+		mkdir -p "${INSTALL_DIR}"
+	fi
 
 	if ! has "wget"; then
 		echo "wget required"
 		exit 1
 	fi
+
+	echo "Start installing z ..."
 
 	wget -O "${INSTALL_ITEM}" "https://raw.githubusercontent.com/rupa/z/master/z.sh"
 else
