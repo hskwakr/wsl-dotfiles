32273caed7af1dba572b0376d12011b35eef1b0d
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-06-21 10:12:41 +0900

    Update install-bat.sh

--- etc/init/install-bat.sh
+++ etc/init/install-bat.sh
@@ -3,16 +3,21 @@
 # https://github.com/sharkdp/bat#on-ubuntu-using-most-recent-deb-packages
 
 DOT_DIR="$HOME/wsl-dotfiles"
-INSTALL_ITEM="${DOT_DIR}/.cache/bat.deb"
+INSTALL_DIR="${DOT_DIR}/.cache"
+INSTALL_ITEM="${INSTALL_DIR}/bat.deb"
 . "${DOT_DIR}/etc/lib/sh/has.sh"
 
 if ! has "bat"; then
-	echo "Start installing bat ..."
+	if [ ! -d "${INSTALL_DIR}" ]; then
+		mkdir -p "${INSTALL_DIR}"
+	fi
 
 	if ! has "wget"; then
 		echo "wget required"
 		exit 1
 	fi
+
+	echo "Start installing bat ..."
 
 	wget -O "${INSTALL_ITEM}" "https://github.com/sharkdp/bat/releases/download/v0.18.1/bat_0.18.1_amd64.deb"
 	# adapt version number and architecture
