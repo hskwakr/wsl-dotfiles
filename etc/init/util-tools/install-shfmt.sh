a9d9532b3c5534349fc16364c86794adb121c4a1
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-06-23 07:35:58 +0900

    Add shfmt

---
+++ etc/init/util-tools/install-shfmt.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Install shfmt with a way following below site.
+# https://github.com/mvdan/sh
+
+DOT_DIR="$HOME/wsl-dotfiles"
+. "${DOT_DIR}/etc/lib/sh/has.sh"
+
+if has "go"; then
+	printf "\n\nStart installing go ..."
+  GO111MODULE=on go get mvdan.cc/sh/v3/cmd/shfmt
+else
+	echo "go required"
+	exit 1
+fi
