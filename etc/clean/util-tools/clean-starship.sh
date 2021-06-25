e779f99b0e615335b94674d82abf564bb1c6cba7
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-06-25 19:59:25 +0900

    Add starship

---
+++ etc/clean/util-tools/clean-starship.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Remove starship with a way following below site.
+# https://starship.rs/faq/#how-do-i-uninstall-starship
+
+printf "\n\n"
+echo "Remove starship ..."
+# Locate and delete the starship binary
+rm "$(which starship)"
---
+++ etc/init/util-tools/install-starship.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Install starship with a way following below site.
+# https://starship.rs/guide/#%F0%9F%9A%80-installation
+
+DOT_DIR="$HOME/wsl-dotfiles"
+. "${DOT_DIR}/etc/lib/sh/has.sh"
+
+if ! has "curl"; then
+  echo "curl required"
+  exit 1
+fi
+
+if fc-list | grep 'Fira' >/dev/null; then
+  sh "${DOT_DIR}/etc/init/util-tools/install-nerd-fonts.sh"
+fi
+
+printf "\n\n"
+echo "Start installing starship ..."
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
