5374ab20fc5098df768df342b24230f5ba5ceb4c
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-06-25 20:43:12 +0900

    Update clean-starship.sh

--- etc/clean/util-tools/clean-starship.sh
+++ etc/clean/util-tools/clean-starship.sh
@@ -2,7 +2,15 @@
 # Remove starship with a way following below site.
 # https://starship.rs/faq/#how-do-i-uninstall-starship
 
-printf "\n\n"
-echo "Remove starship ..."
-# Locate and delete the starship binary
-rm -f "$(which starship)"
+DOT_DIR="$HOME/wsl-dotfiles"
+. "${DOT_DIR}/etc/lib/sh/has.sh"
+
+if has "starship"; then
+  printf "\n\n"
+  echo "Remove starship ..."
+  # Locate and delete the starship binary
+  sudo rm -fv "$(which starship)"
+else
+  echo "starship does not exist"
+  exit 1
+fi
