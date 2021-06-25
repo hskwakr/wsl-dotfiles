6cbe358982454f03ba0ec160fc66348e5330f0df
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-06-25 19:07:13 +0900

    Create clean-installed-fonts.sh

---
+++ etc/clean/util-tools/clean-installed-fonts.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Remove installed fonts
+
+DOT_DIR="$HOME/wsl-dotfiles"
+FONTS_DIR="$HOME/.fonts"
+. "${DOT_DIR}/etc/lib/sh/has.sh"
+
+if [ ! -d "${FONTS_DIR}" ]; then
+  echo "${FONTS_DIR} does not exist"
+  exit 1
+fi
+rm -rf "${FONTS_DIR:?}"
+
+if ! has "fc-cache"; then
+  sh "${DOT_DIR}/etc/init/build-tools/install-fontconfig.sh"
+fi
+fc-cache -fv
