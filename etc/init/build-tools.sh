ea8fa85428895b5755420ae1792bb1f4c84c7499
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-06-25 10:22:36 +0900

    Sparate init scripts to 3 files

---
+++ etc/init/build-tools.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+DOT_DIR="$HOME/wsl-dotfiles"
+SCRIPTS_DIR="${DOT_DIR}/etc/init"
+BUILD_TOOL_DIR="${SCRIPTS_DIR}/build-tools"
+
+. "${DOT_DIR}/etc/lib/sh/run_current_dir_scripts.sh"
+
+if [ -d "${BUILD_TOOL_DIR}" ]; then
+  print "\n\n"
+  echo "Start runing init scripts in build-tools..."
+  cd "${BUILD_TOOL_DIR}" || exit 1
+  run_current_dir_scripts
+else
+  echo "${BUILD_TOOL_DIR} does not exist"
+  exit 1
+fi
