8062f9a6b0bafcc60a8227a697e191a51604d5fd
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-06-27 09:39:45 +0900

    Add tmux-bash-completion

---
+++ etc/clean/util-tools/clean-tmux-bash-completion.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Remove tmux-bash-completion
+
+DOT_DIR="$HOME/wsl-dotfiles"
+INSTALL_DIR="${DOT_DIR}/.cache/bash/completion"
+INSTALL_ITEM="${INSTALL_DIR}/tmux"
+
+if [ -f "${INSTALL_ITEM}" ]; then
+  printf "\n\n"
+  echo "Remove tmux bash completion ..."
+  rm -vf "${INSTALL_ITEM}"
+else
+  echo "${INSTALL_ITEM} does not exist"
+  exit 1
+fi
+
---
+++ etc/init/util-tools/install-tmux-bash-completion.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Install tmux-bash-completion
+
+DOT_DIR="$HOME/wsl-dotfiles"
+INSTALL_DIR="${DOT_DIR}/.cache/bash/completion"
+INSTALL_ITEM="${INSTALL_DIR}/tmux"
+
+. "${DOT_DIR}/etc/lib/sh/has.sh"
+
+if [ -f "${INSTALL_ITEM}" ]; then
+  echo "${INSTALL_ITEM} is already installed"
+  exit 1
+fi
+if ! has "wget"; then
+  echo "wget required"
+  exit 1
+fi
+
+if [ ! -d "${INSTALL_DIR}" ]; then
+  mkdir -p "${INSTALL_DIR}"
+fi
+
+printf "\n\n"
+echo "Start installing tmux bash completion ..."
+wget -O "${INSTALL_ITEM}" "https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/master/completions/tmux"
