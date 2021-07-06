fd9c8606c2dcb8a8334a1a788a46c6b6b30f78ca
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-07-06 19:16:55 +0900

    Update fzf

--- etc/clean/util-tools/clean-fzf.sh
+++ etc/clean/util-tools/clean-fzf.sh
@@ -3,13 +3,18 @@
 # https://github.com/junegunn/fzf#using-linux-package-managers
 
 DOT_DIR="$HOME/wsl-dotfiles"
+INSTALLED_DIR="${DOT_DIR}/.cache/.fzf"
 . "${DOT_DIR}/etc/lib/sh/has.sh"
 
 if has "fzf"; then
-  echo "Remove fzf ..."
-
-  sudo apt update
-  sudo apt -y remove fzf
+  if [ -d "${INSTALLED_DIR}" ]; then
+    echo "Remove fzf ..."
+    rm -rf "${INSTALLED_DIR}"
+    echo "... done"
+  else
+    echo "Could not remove fzf"
+    exit 0
+  fi
 else
   echo "fzf is already removed"
 fi
--- etc/init/util-tools/install-fzf.sh
+++ etc/init/util-tools/install-fzf.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 # Install fzf with a way following below site.
-# https://github.com/junegunn/fzf#using-linux-package-managers
+# https://github.com/junegunn/fzf#using-git
 
 DOT_DIR="$HOME/wsl-dotfiles"
+INSTALL_DIR="${DOT_DIR}/.cache/.fzf"
 . "${DOT_DIR}/etc/lib/sh/has.sh"
 
 if ! has "fzf"; then
   printf "\n\n"
   echo "Start installing fzf ..."
 
-  sudo apt update
-  sudo apt -y install fzf
+  git clone --depth 1 https://github.com/junegunn/fzf.git "${INSTALL_DIR}"
+  bash -c "${INSTALL_DIR}/install --bin --no-bash"
 else
   echo "fzf is already installed"
 fi
