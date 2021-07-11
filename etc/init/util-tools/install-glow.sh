560d34f8db9accdf6de604b555e5ddceb2d10b95
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-07-11 21:18:59 +0900

    Create bin dir to store executable files

--- etc/init/util-tools/install-glow.sh
+++ etc/init/util-tools/install-glow.sh
@@ -3,16 +3,22 @@
 # https://github.com/charmbracelet/glow#installation
 
 DOT_DIR="$HOME/wsl-dotfiles"
-INSTALL_DIR="${DOT_DIR}/.cache/.glow"
+BIN_DIR="${DOT_DIR}/.cache/bin"
+DOWNLOAD_DIR="${DOT_DIR}/.cache/.glow"
 . "${DOT_DIR}/etc/lib/sh/has.sh"
 
 if ! has "glow"; then
   printf "\n\n"
   echo "Start installing glow ..."
 
-  git clone --depth 1 https://github.com/charmbracelet/glow.git "${INSTALL_DIR}"
-  cd "${INSTALL_DIR}" || exit 0
+  git clone --depth 1 https://github.com/charmbracelet/glow.git "${DOWNLOAD_DIR}"
+  cd "${DOWNLOAD_DIR}" || exit 0
   go build
+
+  if [ ! -d "${BIN_DIR}" ]; then
+    mkdir -p "${BIN_DIR}"
+  fi
+  ln -snf "${DOWNLOAD_DIR}/glow" "${BIN_DIR}/glow"
 else
   echo "glow is already installed"
 fi
