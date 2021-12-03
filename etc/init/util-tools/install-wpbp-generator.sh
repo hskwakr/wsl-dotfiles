02f4fc2d6178bc2ecf15bb8086ca178182f51421
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-12-04 04:08:35 +0900

    Add wpbp-generator

---
+++ etc/init/util-tools/install-wpbp-generator.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Install wpbp-generator with a way following below site.
+# https://github.com/WPBP/generator
+
+DOT_DIR="$HOME/wsl-dotfiles"
+. "${DOT_DIR}/etc/lib/sh/has.sh"
+
+if ! has "wpbp-generator"; then
+  if [ ! -d "${INSTALL_DIR}" ]; then
+    mkdir -p "${INSTALL_DIR}"
+  fi
+
+  if ! has "composer"; then
+    echo "composer required"
+    exit 1
+  fi
+
+  printf "\n\n"
+  echo "Start installing wpbp-generator ..."
+
+  composer global require wpbp/generator:dev-master
+else
+  echo "wpbp-generator is already installed"
+fi
